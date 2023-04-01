3e35aadd9011ab361cdb5a56f6818221b142845b
Nicholas Martin-Kruck <nickmartink@gmail.com>
2023-04-01 15:14:30 +0100

    Login tweaks

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { StytchLogin, useStytch, useStytchSession } from "@stytch/nextjs";
 import { OAuthProviders, OneTapPositions, Products } from "@stytch/vanilla-js";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const config = {
   oauthOptions: {
@@ -18,22 +18,39 @@ const config = {
 export default function LoginPage() {
   const stytchClient = useStytch();
   const { session } = useStytchSession();
+  const [error, setError] = useState<string>();
+  const [token, setToken] = useState<string | null>();
 
   useEffect(() => {
+    setToken(new URLSearchParams(window.location.search).get("token"));
     if (session) {
       window.location.href = "/dashboard";
-    } else {
-      const token = new URLSearchParams(window.location.search).get("token");
-      token &&
-        stytchClient.oauth.authenticate(token, {
+    } else if (token) {
+      stytchClient.oauth
+        .authenticate(token, {
           session_duration_minutes: 60,
+        })
+        .catch((e) => {
+          console.log("error", e);
+          setError(e.message);
         });
     }
   }, [stytchClient, session]);
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2 -mt-24 sm:px-6 lg:px-8">
-      <StytchLogin config={config} />
+    <div className="flex flex-col items-center justify-center min-h-screen py-2 -mt-24 sm:px-6 lg:px-8 mx-auto">
+      {error && (
+        <div className="rounded-lg py-6 px-4 bg-red-300 text-red-800 my-4">
+          <p>{error}</p>
+          <p>
+            <strong>Please try again.</strong>
+          </p>
+        </div>
+      )}
+      <div className="text-white">
+        {token && !error && <h3 className="">Logging in...</h3>}
+      </div>
+      {(!token || error) && <StytchLogin config={config} />}
     </div>
   );
 }
