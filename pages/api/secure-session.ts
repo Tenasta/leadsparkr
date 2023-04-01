30d3bec50b45a4441e5e82211b253049540043e1
Nicholas Martin-Kruck <nickmartink@gmail.com>
2023-04-01 14:09:06 +0100

    Upsert user

--- pages/api/secure-session.ts
+++ pages/api/secure-session.ts
@@ -1,4 +1,5 @@
 import { authenticateStytchSession } from "@/lib/authenticateStytchSession";
+import prisma from "@/lib/prisma";
 import { NextApiRequest } from "next";
 
 export default async function withAuthSession(req: NextApiRequest) {
@@ -7,9 +8,25 @@ export default async function withAuthSession(req: NextApiRequest) {
   );
   if (!session) return false;
 
-  return {
-    user: {
+  const email = session.user.emails.find((e) => e.verified)?.email;
+  if (!email) throw new Error("No verified email found");
+
+  const userData = {
+    email,
+    first_name: session.user.name.first_name,
+    last_name: session.user.name.last_name,
+  };
+
+  const user = await prisma.user.upsert({
+    where: { id: session.user.user_id },
+    create: {
       id: session.user.user_id,
+      ...userData,
     },
+    update: userData,
+  });
+
+  return {
+    user,
   };
 }
