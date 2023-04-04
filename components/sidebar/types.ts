208874d079cd0d4883d423e159f7610352d8034b
Nicholas Martin-Kruck <nickmartink@gmail.com>
2023-04-04 14:09:19 +0100

    Fix

--- components/sidebar/types.ts
+++ components/sidebar/types.ts
@@ -1,5 +1,12 @@
+type HeroIcon = React.ForwardRefExoticComponent<
+  React.PropsWithoutRef<React.SVGProps<SVGSVGElement>> & {
+    title?: string;
+    titleId?: string;
+  } & React.RefAttributes<SVGSVGElement>
+>;
+
 export type NavItem = {
   name: string;
   href: string;
-  icon: React.ComponentType<React.RefAttributes<SVGSVGElement>>;
+  icon: HeroIcon;
 };
